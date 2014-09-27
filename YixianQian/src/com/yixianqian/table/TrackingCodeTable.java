3290f23ff94fe08e2d0eda90db1b08e60c82428d
xmuzhangshuai <1207509737@qq.com>
2014-09-27 11:36:05 +0800

    邀请码

---
+++ YixianQian/src/com/yixianqian/table/TrackingCodeTable.java
@@ -0,0 +1,11 @@
+package com.yixianqian.table;
+
+public class TrackingCodeTable {
+	
+	public static final String 	TABLE_NAME = "tc_trackingcode";
+	public static final String 	TC_ID = "tc_id";
+	public static final String 	TC_HOSTERID = "tc_hosterid";
+	public static final String 	TC_RECEIVERID = "tc_receiverid";
+	public static final String 	TC_CODE = "tc_code";
+	public static final String 	TC_TIME = "tc_time";
+}
